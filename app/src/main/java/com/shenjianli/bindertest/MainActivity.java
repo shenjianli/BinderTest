4c07f738df9bd9970d3915cb7a0367fd92ee4eae
shenjianli <cqtddt@163.com>
2016-11-18 23:07:02 +0800

    增加使用Binder进行与Service通信，获取Service进程中的结果

--- app/src/main/java/com/shenjianli/bindertest/MainActivity.java
+++ app/src/main/java/com/shenjianli/bindertest/MainActivity.java
@@ -1,13 +1,74 @@
 package com.shenjianli.bindertest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.open_account)
+    Button mOpenAccount;
+    @Bind(R.id.save_money)
+    Button mSaveMoney;
+    @Bind(R.id.get_money)
+    Button mGetMoney;
+    @Bind(R.id.close_account)
+    Button mCloseAccount;
+    @Bind(R.id.text_info)
+    TextView mTextInfo;
+    private BankBinder mBankBinder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        bindService(new Intent("com.shenjianli.aidl.bank.BankService"),conn,BIND_AUTO_CREATE);
+    }
+
+    private ServiceConnection conn = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder binder) {
+            mBankBinder = (BankBinder) binder;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
+
+    @OnClick({R.id.open_account, R.id.save_money, R.id.get_money, R.id.close_account})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.open_account:
+                mTextInfo.setText(mBankBinder.openAccount("shenjianli","cqtddt@163.com"));
+                break;
+            case R.id.save_money:
+                mTextInfo.setText(mBankBinder.saveMoney(1000000000,"shenjianli"));
+                break;
+            case R.id.get_money:
+                mTextInfo.setText(mBankBinder.takeMoney(100,"shenjianli","cqtddt@163.com"));
+                break;
+            case R.id.close_account:
+                mTextInfo.setText(mBankBinder.closeAccount("shenjianli","cqtddt@163.com"));
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbindService(conn);
     }
 }
